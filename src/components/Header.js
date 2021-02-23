46943efa6ce03260d99804825a759aacbaaea277
maryam <it.lancer999@gmail.com>
2021-02-23 17:01:32 -0500

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/header.css';
+
+const CardHeader = () => (
+  <div className="card__header--title">
+    <h1>Create Account</h1>
+  </div>
+)
+
+export default CardHeader
+
+
---
+++ src/components/Image-pane.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ImagePane = () => {
+  return (
+    <div className="card__image">
+      <img
+        className="card__image--credits" width="455px" height="520px"
+        src="./Images/dribble.jpg" alt="side"
+      />
+
+      
+    </div>
+  );
+};
+
+export default ImagePane;
